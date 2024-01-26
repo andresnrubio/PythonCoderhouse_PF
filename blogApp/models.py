f4e19482062bec5aa80d39874c69589d634c39b2
Andres Rubio <rubioandres1@gmail.com>
2024-01-25 21:07:36 -0300

    subida parcial funcionando sin templates

--- blogApp/models.py
+++ blogApp/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+class blogEntry(models.Model):
+    title = models.CharField(max_length=120)
+    imgUrl = models.CharField(max_length=100)
+    date = models.DateField()
+    content = models.CharField(max_length=5000)
+
+
+class comment(models.Model):
+    username = models.CharField(max_length=50)
+    comment = models.CharField(max_length=400)
+    date = models.DateField()
+
+
+class user(models.Model):
+    username = models.CharField(max_length=50)
+    name = models.CharField(max_length=50)
+    lastname = models.CharField(max_length=50)
+    email = models.EmailField(max_length=254)
--- blogApp/views.py
+++ blogApp/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import HttpResponse
+from django.template import loader
+
 
 # Create your views here.
+def home(request):
+    template = loader.get_template("home.html")
+
+    dictionary = {}
+
+    document = template.render(dictionary)
+
+    return HttpResponse(document)
